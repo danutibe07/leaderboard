52c11cd31efe2e3cc34ad769f907c22c3603cc6d
danutibe07 <danielutibe07@gmail.com>
2023-03-14 06:04:20 +0100

    implementing the submit button

---
+++ src/modules/submitScores.js
@@ -0,0 +1,20 @@
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zcAQ2OYLej6OHSOIzBVm/scores/';
+
+const submitScore = async (user, score) => {
+    const gameOptions = {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        user,
+        score,
+      }),
+    };
+  
+    const dataStream = await fetch(`${url}`, gameOptions);
+    const dataResponse = await dataStream.json();
+    return dataResponse;
+  };
+
+  export default submitScore;
